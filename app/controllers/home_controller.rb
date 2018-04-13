b8d48f2b2c74c70147c024e43397f9ea9fd3b137
Debbie Pao <dpao@berkeley.edu>
2018-04-12 23:18:05 -0700

    yay map works!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
 
   def index
     @user = current_user
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
   end
 
 end
