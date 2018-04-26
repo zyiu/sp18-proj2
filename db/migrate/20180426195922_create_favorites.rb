8676a274a26185b8a3b76e0a27f7705632d5cd92
Debbie Pao <dpao@berkeley.edu>
2018-04-26 14:45:46 -0700

    fixed favorites

---
+++ db/migrate/20180426195922_create_favorites.rb
@@ -0,0 +1,9 @@
+class CreateFavorites < ActiveRecord::Migration[5.1]
+  def change
+    create_table :favorites do |t|
+      t.string :listing_id
+      t.string :user_id
+      t.timestamps
+    end
+  end
+end
